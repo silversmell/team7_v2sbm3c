957dd1152e9c5f375d7cde1fc52fc0e4fa9b6696
tnwls <kim20267878@gmail.com>
2024-05-16 18:14:40 +0900

    For backup

--- src/main/java/dev/mvc/team7_v2sbm3c/DatabaseConfiguration.java
+++ src/main/java/dev/mvc/team7_v2sbm3c/DatabaseConfiguration.java
@@ -18,7 +18,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @PropertySource("classpath:/application.properties")  // 설정 파일 위치
-@MapperScan(basePackages= {"dev.mvc.share_contents","dev.mvc.category","dev.mvc.admin","dev.mvc.qna_contents"}) // DAO interface를 찾는 위치
+@MapperScan(basePackages= {"dev.mvc.share_contents","dev.mvc.category","dev.mvc.qna_contents"}) // DAO interface를 찾는 위치
 
 public class DatabaseConfiguration {
     
