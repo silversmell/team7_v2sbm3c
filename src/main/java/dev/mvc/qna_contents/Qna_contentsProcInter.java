ca542ad92b0abdf185d2bb53e1aace7096d52cfb
tnwls <kim20267878@gmail.com>
2024-05-16 15:14:12 +0900

    qna_contentsProcInter 수정

--- src/main/java/dev/mvc/qna_contents/Qna_contentsProcInter.java
+++ src/main/java/dev/mvc/qna_contents/Qna_contentsProcInter.java
@@ -1,15 +1,21 @@
 package dev.mvc.qna_contents;
 
+import java.util.ArrayList;
+
 public interface Qna_contentsProcInter {
   
   /**
    * 질문게시글 등록
-   * insert id="qna_create" parameterType="dev.mvc.
+   * insert id="qna_create" parameterType="dev.mvc.qna_contents"
    * @param qna_contentsVO
    * @return
    */
   public int create(Qna_contentsVO qna_contentsVO);
 
-  
-  public ArrayList<Qna_contentsVO> read(qcon_no);
+  /**
+   * 질문게시글 전체 목록
+   * insert id="qna_list_all" resultTyle="dev.mvc.qna_contents.Qna_contentsVO"
+   * @return
+   */
+  public ArrayList<Qna_contentsVO> list_all();
 }
