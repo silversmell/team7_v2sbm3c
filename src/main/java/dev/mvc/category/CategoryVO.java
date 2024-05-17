069aa15e96415f4a1d3d91065139547aefbdc492
tnwls <kim20267878@gmail.com>
2024-05-17 16:08:53 +0900

    카테고리 등록 및 검색 가능

--- src/main/java/dev/mvc/category/CategoryVO.java
+++ src/main/java/dev/mvc/category/CategoryVO.java
@@ -32,17 +32,17 @@ public class CategoryVO {
   @NotNull(message="관련 자료 수는 필수입력 항목입니다.")
   @Min(value=0)
   @Max(value=1000000)
-  private Integer cnt = 0;
+  private Integer cate_cnt = 0;
   
   /** 카테고리 출력 순서 */
   @NotNull(message="출력 순서는 필수입력 항목입니다.")
   @Min(value=0)
   @Max(value=1000000)
-  private Integer seqno = 0;
+  private Integer cate_seqno;
   
   /** 카테고리 출력 모드 */
   @NotNull(message="출력 모드는 필수입력 항목입니다.")
   @Pattern(regexp="^[YN]$", message="Y 또는 N만 입력 가능합니다.")
-  private String visible = "Y";
+  private String cate_visible = "Y";
   
 }
