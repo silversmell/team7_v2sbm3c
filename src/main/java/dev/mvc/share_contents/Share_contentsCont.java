51fc4b4789c320e08f2d9009831c8ac9379f87ae
silversmell <ssm06091@naver.com>
2024-06-25 09:29:56 +0900

    공유글 수정, 삭제시 관리자 포함

--- src/main/java/dev/mvc/share_contents/Share_contentsCont.java
+++ src/main/java/dev/mvc/share_contents/Share_contentsCont.java
@@ -353,7 +353,7 @@ public class Share_contentsCont {
 	@GetMapping("/update_text/{member_no}") // 글 수정
 	public String update_text_form(Model model,@PathVariable("member_no") Integer acc_no, int scon_no, int cate_no,HttpSession session) {
 
-		if(this.accountProc.isMember(session) && (acc_no==(int)session.getAttribute("acc_no"))) {
+		if(this.accountProc.isMemberAdmin(session) || (acc_no==(int)session.getAttribute("acc_no"))) {
 			System.out.println("들어옴");
 		CategoryVO categoryVO = this.categoryProc.cate_read(cate_no);
 		model.addAttribute("categoryVO", categoryVO);
@@ -642,7 +642,7 @@ public class Share_contentsCont {
 		if(acc_no==0) {
 			return "account/login";
 		}
-		if(this.accountProc.isMember(session) && ((int)session.getAttribute("acc_no")==acc_no)){
+		if(this.accountProc.isMemberAdmin(session) || ((int)session.getAttribute("acc_no")==acc_no)){
 		CategoryVO categoryVO = this.categoryProc.cate_read(cate_no);
 		model.addAttribute("categoryVO", categoryVO);
 
